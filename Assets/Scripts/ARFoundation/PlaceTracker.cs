d572b0dbdb95056bcb7f14575cafff0e4e17786b
EunhoKang <1999paul@naver.com>
2022-06-09 12:57:42 +0900

    Team work merging

--- Assets/Scripts/ARFoundation/PlaceTracker.cs
+++ Assets/Scripts/ARFoundation/PlaceTracker.cs
@@ -9,7 +9,6 @@ public class PlaceTracker : MonoBehaviour
     public Room sampleRoom;
     public GameObject canvas;
     [HideInInspector]public Room currentRoom=null;
-    public ARAnchorManager anchorManager;
 
     Vector3 defaultPivot=new Vector3(0,-0.5f,0);
     
@@ -17,11 +16,6 @@ public class PlaceTracker : MonoBehaviour
         if(!currentRoom) {
             currentRoom=Instantiate(sampleRoom);
         }
-        
-        var anchor=anchorManager.AddAnchor(
-            new Pose(Camera.main.transform.position-currentRoom.playerPoint.position,
-            Quaternion.LookRotation(currentRoom.playerPoint.position-currentRoom.targetPoint.position)));
-        currentRoom.transform.parent=anchor.transform;
     
         currentRoom.transform.rotation=Quaternion.LookRotation(currentRoom.playerPoint.position-currentRoom.targetPoint.position);
         currentRoom.transform.position=Camera.main.transform.position-currentRoom.playerPoint.position+defaultPivot;
